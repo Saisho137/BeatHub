7df186852040b80f67e2dc32e3afadb42253e596
unknown <juanjosesierram@gmail.com>
2023-06-15 14:28:01 -0500

    feat: controller returns top tracks

--- Backend_BeatHub/controllers/statisticsController.js
+++ Backend_BeatHub/controllers/statisticsController.js
@@ -17,14 +17,13 @@ const getStats = async (req, res) => {
         return { id, display_name, followers, images, country }
     }
 
-    const getTopArtistsAndGenres = async (id) => {
+    const getTopArtistsAndGenres = async () => {
 
         const genresList = {}
 
-        const { data: { items } } = await axios.get('https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=10&offset=0', headers)
+        const { data: { items } } = await axios.get(`https://api.spotify.com/v1/me/top/artists?time_range=${time}&limit=10&offset=0`, headers)
 
         items.map(({ genres }) => {
-            console.log(genres);
             for (const genre of genres) {
                 if (genresList.hasOwnProperty(genre)) {
                     genresList[genre]++
@@ -36,15 +35,23 @@ const getStats = async (req, res) => {
         })
 
         return {
-            artists: items.map(({ id, images, name }) => ({ id, images: images[0], name })),
+            artists: items.map(({ id, name, images }) => ({ id, name, images: images[0] })),
             genres: genresList
         }
     }
 
+    const getTopTracks = async () => {
+        const { data: { items } } = await axios.get(`https://api.spotify.com/v1/me/top/tracks?time_range=${time}&limit=10&offset=0`, headers)
+        return items.map(({ id, name, album, artists }) => ({ id, name, artists: artists.map(({ name }) => ( name )), images: album.images[0] }))
+    }
+
     const user = await getUserProfile()
     const topArtistsAndGenres = await getTopArtistsAndGenres()
+    const topTracks = await getTopTracks()
 
-    res.status(200).send({ user, ...topArtistsAndGenres })
+    console.log(topTracks);
+
+    res.status(200).send({ user, ...topArtistsAndGenres, topTracks })
 }
 
 module.exports = { getStats }
