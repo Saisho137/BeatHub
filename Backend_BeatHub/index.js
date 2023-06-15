71a49e2c9965584ed6a864a3bbf11d24781448f4
Saisho <santib.2001@hotmail.com>
2023-06-14 19:37:06 -0500

    feat: First version of Spotify Login added

--- Backend_BeatHub/index.js
+++ Backend_BeatHub/index.js
@@ -1,17 +1,6 @@
 'use strict'
-
-const mongoose = require('mongoose')
 const app = require('./app')
 
-mongoose.connect('mongodb://127.0.0.1:27017/BeatHubDB')
-    .then(
-        () => {
-            console.log("Succesful connection with DDBB.")
-            app.listen(8698, function () {
-                console.log("Server has been Initilized.")
-            })
-        })
-    .catch((err) => {
-        console.log("Failed to connect to DDBB.")
-        console.log(err)
-    })
+app.listen(8080, function () {
+    console.log("Server has been Initilized.")
+})
--- Backend_BeatHub/routes/routes.js
+++ Backend_BeatHub/routes/routes.js
@@ -1,4 +1,10 @@
 'use strict'
+
 const express = require('express') 
 const app = express.Router()
+const loginSpotifyController = require('../controllers/loginController')
+
+app.get('/login', loginSpotifyController.loginSpotifyUser)
+app.get('/callback', loginSpotifyController.loginSpotifyCallback)
+
 module.exports = app
