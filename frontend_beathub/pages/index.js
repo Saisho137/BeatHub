6e4aed8ef0ff4f5762c107d728df935bb226413e
unknown <juanjosesierram@gmail.com>
2023-06-14 20:33:34 -0500

    fix: Moved Spotify login to Frontend

--- frontend_beathub/pages/index.js
+++ frontend_beathub/pages/index.js
@@ -1,7 +1,33 @@
 import Layout from "../components/layout";
+import { useEffect } from 'react'
 
 export default function Home() {
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem('token')
+
+    if (!token && hash) {
+      const urlParams = new URLSearchParams(window.location.hash.replace('#','?'))
+      let token = urlParams.get('access_token')
+
+      window.location.hash = ''
+      window.localStorage.setItem('token',token)
+    }
+  }, [])
+
+  const CLIENT_ID = '39dd8906e8044b7eb1715c1a4a1867e7'
+  const REDIRECT_URI = "http://localhost:3000"
+  const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
+  const RESPONSE_TYPE = 'token'
+  const SCOPE = 'user-read-private playlist-read-private user-read-currently-playing user-follow-read'
+
   return (
-    <Layout></Layout>
-  )
+    <Layout>
+      <a 
+            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`}>
+            Login with Spotify
+          </a>
+    </Layout>
+  );
 }
