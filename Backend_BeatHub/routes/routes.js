6c0285ce087f882629babfd2158656a0b285a60e
Saisho <santib.2001@hotmail.com>
2023-06-16 00:29:04 -0500

    feat: Game controller functionality v 1.0

--- Backend_BeatHub/routes/routes.js
+++ Backend_BeatHub/routes/routes.js
@@ -3,4 +3,8 @@
 const express = require('express') 
 const app = express.Router()
 
+const gameController = require('../controllers/gameController')
+
+app.post('/getRandomTrack', gameController.randomSong)
+
 module.exports = app
