05619907dfa903c0872ca12e73a56f147b0efdce
daiko <mirole16@gmail.com>
2023-06-17 22:39:06 -0500

    Feat: All recommender endpoints finished

--- Backend_BeatHub/routes/routes.js
+++ Backend_BeatHub/routes/routes.js
@@ -5,8 +5,19 @@ const recommenderController = require('../controllers/recommenderController')
 const express = require('express') 
 const app = express.Router()
 
-//Get artist
-app.get('/getTrack', recommenderController.getTrack)
+//Artis
+app.get('/getArtist/:artist', recommenderController.getArtist)
+app.get('/getSpecificArtist/:id', recommenderController.getSpecificArtist)
+app.get('/getSimilarArtist/:id', recommenderController.getSimilarArtist)
+app.get('/getArtistTopTracks/:id', recommenderController.getArtistTopTracks)
+//Track
+app.get('/getTrack/:track', recommenderController.getTrack)
+app.get('/getSpecificTrack/:id', recommenderController.getSpecificTrack)
+app.get('/getSimilarTracks', recommenderController.getSimilarTrack)
+//Genre
+app.get('/getGenre', recommenderController.getGenre)
+app.get('/getTopTracksGenre/:genre', recommenderController.getTopTracksGenre)
+app.get('/getTopArtistGenre/:genre', recommenderController.getTopArtistGenre)
 
 
 module.exports = app
