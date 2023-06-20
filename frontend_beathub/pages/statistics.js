d04a15211e9866095bd783ef3e922b2f409cc500
unknown <juanjosesierram@gmail.com>
2023-06-19 21:49:57 -0500

    feat: Added user's top items

--- frontend_beathub/pages/statistics.js
+++ frontend_beathub/pages/statistics.js
@@ -32,23 +32,52 @@ export default function Statistics() {
 
     }, [time])
 
-    console.log(stats);
     if (!stats.user) {
         return <p>Loading</p>
     }
 
+    console.log(stats)
+
     return (
-        <div className='container h-100'>
-            <div className='card mb-3 col-md-4 align-middle'>
+        <div className='row'>
+            {/* User's Profile */}
+            <div className='card mb-3 col-md-4 offset-md-1 align-middle'>
                 <div className='row align-items-center h-100'>
                     <div className='col-md-4'>
-                        <img src={stats.user?.images[0] ? stats.user.images[0].url : 'images/person-circle.svg'} className='img-fluid rounded-circle' alt='Bootstrap' />
+                        <img src={stats.user?.images[0] ? stats.user.images[0].url : 'images/person-circle.svg'}
+                            className='img-fluid rounded-circle'
+                            alt='User Profile Picture'
+                        />
                     </div>
                     <div className='col-md-8'>
-                        <div class="card-body">
-                            <h5 class="card-title">{stats.user.display_name}</h5>
-                            <p>followers: {stats.user.followers.total}</p>
+                        <div className="card-body">
+                            <h5 className='card-title'>{stats.user.display_name}</h5>
+                            <p className='card-text'>followers: {stats.user.followers.total}</p>
                         </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* User's Top Items */}
+            <div className='col-md-7 row'>
+                <div className='col-md-3 offset-md-1 card text-center border-0'>
+                    <h5>Top Track</h5>
+                    <img src={stats.tracks[0]?.images.url} alt='Artist Picture' />
+                    <div class="card-body">
+                        <h5 className="card-title">{stats.tracks[0].name}</h5>
+                    </div>
+                </div>
+                <div className='col-md-3 card text-center border-0'>
+                    <h5>Top Genre(s)</h5>
+                    {stats.genres
+                        .filter(genre => genre[1] === Math.max(...stats.genres.map(genre => genre[1])))
+                        .map(genre => <h6>{genre[0]}</h6>)}
+                </div>
+                <div className='col-md-3 card text-center border-0'>
+                    <h5>Top Artist</h5>
+                    <img src={stats.artists[0]?.images.url} alt='Artist Picture' />
+                    <div class="card-body">
+                        <h5 className="card-title">{stats.artists[0].name}</h5>
                     </div>
                 </div>
             </div>
