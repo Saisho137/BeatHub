c9fe918fdf3754ba48e63170f3d91ada2e8db727
Saisho <santib.2001@hotmail.com>
2023-06-25 16:29:47 -0500

    feat: finished game page template

---
+++ frontend_beathub/pages/game.js
@@ -0,0 +1,14 @@
+import Layout from "../components/layout"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import Image from "next/image"
+
+const Game = () => {
+    return (
+        <Layout>
+            <h1>Test</h1>
+        </Layout>
+    )
+}
+
+export default Game
