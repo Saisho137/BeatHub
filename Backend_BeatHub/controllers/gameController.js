5e9f599673889a2e5cea723e4e0e75f06a43d3c5
Saisho <santib.2001@hotmail.com>
2023-06-26 23:40:08 -0500

    feat: finished basic functionalities and fixed response from backend

--- Backend_BeatHub/controllers/gameController.js
+++ Backend_BeatHub/controllers/gameController.js
@@ -9,6 +9,14 @@ const getRandomSong = async (req, res) => {
         const searchItem = body.searchItem
         const trackList = []
 
+        const token = req.headers.authorization
+
+        const headers = {
+            headers: {
+                'Authorization': token
+            }
+        }
+
         if (!findBy || !searchItem) {
             res.status(400).send({ Message: "Bad Request" })
             return
@@ -16,26 +24,20 @@ const getRandomSong = async (req, res) => {
 
         switch (findBy) {
             case "genre":
-                const { data } = await axios.get(`https://api.spotify.com/v1/search?query=genre%3A${searchItem}&type=track`,
-                    { headers: { Authorization: `${req.headers.authorization}` } }
-                )
+                const { data } = await axios.get(`https://api.spotify.com/v1/search?query=genre%3A${searchItem}&type=track`, headers)
                 data.tracks.items.map((track) => {
                     trackList.push(track.id)
                 })
                 break
             case "playlist":
                 const playlistID = searchItem.split('playlist/')[1]
-                const playlist = await axios.get(`https://api.spotify.com/v1/playlists/${playlistID}`,
-                    { headers: { Authorization: `${req.headers.authorization}` } }
-                )
+                const playlist = await axios.get(`https://api.spotify.com/v1/playlists/${playlistID}`, headers)
                 playlist.data.tracks.items.map(pl => {
                     trackList.push(pl.track.id)
                 })
                 break
             case "artist":
-                const topTracksArtist = await axios.get(`https://api.spotify.com/v1/search?query=artist%3A${searchItem}&type=track`,
-                    { headers: { Authorization: `${req.headers.authorization}` } }
-                )
+                const topTracksArtist = await axios.get(`https://api.spotify.com/v1/search?query=artist%3A${searchItem}&type=track`, headers)
                 topTracksArtist.data.tracks.items.map((track) => {
                     trackList.push(track.id)
                 })
@@ -44,9 +46,16 @@ const getRandomSong = async (req, res) => {
                 res.status(400).send({ Message: "Bad Request" })
                 return
         }
-        
         const track = trackList[Math.floor(Math.random() * trackList.length)]
-        res.status(200).send({ Track: track })
+
+        const { data } = await axios.get(`https://api.spotify.com/v1/tracks/${track}`, headers)
+        const infoTrack = {
+            name: data.name,
+            artistName: data.album.artists[0].name,
+            image: data.album.images[0].url,
+            preview: data.preview_url
+        }
+        res.status(200).send({ Track: infoTrack })
 
     } catch (err) {
         res.status(401).send({ message: 'Token missing or invalid.' })
--- frontend_beathub/pages/game.js
+++ frontend_beathub/pages/game.js
@@ -4,45 +4,98 @@ import axios from "axios"
 import Image from "next/image"
 
 const Game = () => {
-    const [response, setResponse] = useState('')
-    const [trys, setTrys] = useState('0')
-    //intentos useSatate
+    const [response, setResponse] = useState({})
+    const [trys, setTrys] = useState('3')
+    const [selectedOption, setSelectedOption] = useState('genre')
 
-    const getRandomTrack = () => {
-    }
+    
 
     const handleSubmit = (event) => {
         event.preventDefault()
 
-        const songName = event.target.songName.value;
-        const artist = event.target.artistName.value;
-        console.log(songName, artist);
+        const songName = event.target.songName.value
+        const artist = event.target.artistName.value
+        if (songName.toLowerCase() == response.name.toLowerCase() && artist.toLowerCase() == response.artistName.toLowerCase()) {
+            alert("YOU WIN!!!")
+        } else {
+            alert("KEEP TRYING!")
+        }
+    }
 
+    const handleSubmitTrack = async (event) => {
+        event.preventDefault()
+
+        const token = sessionStorage.getItem('token')
+        const headers = {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }
+
+        switch (selectedOption) {
+            case "genre":
+                const { data } = await axios.post('http://localhost:8080/getRandomTrack', { findBy: selectedOption, searchItem: event.target.genre.value }, headers)
+                setResponse(data.Track)
+                break
+            case "playlist":
+                const playlist = await axios.post('http://localhost:8080/getRandomTrack', { findBy: selectedOption, searchItem: event.target.playlist.value }, headers)
+                setResponse(playlist.data.Track)
+                break
+            case "artist":
+                const artist = await axios.post('http://localhost:8080/getRandomTrack', { findBy: selectedOption, searchItem: event.target.artist.value }, headers)
+                setResponse(artist.data.Track)
+                break
+            default:
+                console.log("Default")
+        }
+    }
+
+    const handleSelectionChange = (event) => {
+        setSelectedOption(event.target.value)
     }
 
     return (
         <Layout>
             <div className="row">
-                <div className="col-5 d-flex justify-content-center border mt-4">
-                    <h2>-------- HOW TO PLAY GUESS THE SONG ----------
-                        First of all, you need to ...
-                    </h2>
+                <div className="row col-5 d-flex justify-content-center border mt-4">
+                    <label className="col-3" htmlFor="typeOfSearch"><b>Select Type of Search:</b></label>
+                    <select className="col-7" id="typeOfSearch" value={selectedOption} onChange={handleSelectionChange}>
+                        <option value="genre">Genre</option>
+                        <option value="playlist">Playlist</option>
+                        <option value="artist">Artist</option>
+                    </select>
+                    <form className="row col-12 d-flex justify-content-center" onSubmit={handleSubmitTrack}>
+                        {selectedOption == "genre" && <>
+                            <label className="col-3" htmlFor="genre"><b>Genre:</b></label>
+                            <input className="col-7" type="text" id="genre" name="genre" required />
+                        </>}
+                        {selectedOption == "playlist" && <>
+                            <label className="col-3" htmlFor="playlist"><b>Playlist:</b></label>
+                            <input className="col-7" type="text" id="playlist" name="playlist" required />
+                        </>}
+                        {selectedOption == "artist" && <>
+                            <label className="col-3" htmlFor="artist"><b>Artist:</b></label>
+                            <input className="col-7" type="text" id="artist" name="artist" required />
+                        </>}
+                        <button className="col-4" type="submit">Find</button>
+                    </form>
                 </div>
-                <div className="row offset-1 col-6  mt-4">
+                <div className="row offset-1 col-6  mt-4 border">
                     <form className="row col-12 border" onSubmit={handleSubmit}>
                         <label className="col-2" htmlFor="songName"><b>Song Name:</b></label>
                         <input className="col-4" type="text" id="songName" name="songName" required />
-                        <h3 className="col-4 offset-1 d-flex justify-content-center">Trys: {trys}</h3>
+                        <h4 className="col-4 offset-1 d-flex justify-content-center">Trys: {trys}</h4>
                         <label className="col-2"><b>Artist Name:</b></label>
                         <input className="col-4" type="text" id="artistName" name="artistName" required />
-                        <button className="col-4 offset-1" type="submit">Find</button>
+                        <button className="col-4 offset-1" type="submit">Try!</button>
                     </form>
-                    {response != undefined ?
+                    {response.name ?
                         <div className="col-6 offset-3 mt-3 border">
-                            <audio className="col-12" controls src="https://p.scdn.co/mp3-preview/e73f8be53d4445b195c3c22a44bc0e65daf2bf59?cid=0b297fa8a249464ba34f5861d4140e58"></audio>
+                            <audio className="col-12" controls src={response.preview}></audio>
                         </div>
                         :
-                        <p>Si no</p>}
+                        <h1 className="col-6 offset-3 mt-3 d-flex justify-content-center border"> PROVICIONAL </h1>}
+                    {console.log(response)}
                 </div>
             </div>
         </Layout>
