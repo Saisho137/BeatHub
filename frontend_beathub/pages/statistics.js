406ed5a7079633eec5691f256fb32885fa8224a6
unknown <juanjosesierram@gmail.com>
2023-06-26 22:04:46 -0500

    style: Added spinner loader

--- frontend_beathub/pages/statistics.js
+++ frontend_beathub/pages/statistics.js
@@ -34,9 +34,6 @@ export default function Statistics() {
 
     }, [time])
 
-    if (!stats.user) {
-        return <p>Loading</p>
-    }
 
     console.log(stats)
 
@@ -57,124 +54,131 @@ export default function Statistics() {
                     onClick={() => setTime('long_term')}>All Time</button>
             </div>
 
-            <div className='row'>
-                {/* User's Profile */}
-                <div className='card col-md-4 offset-md-1 align-middle text-bg-light mb-5 border-dark mt-5'>
-                    <div className='row align-items-center h-100'>
-                        <div className='col-md-4 h-50'>
-                            <img src={stats.user?.images[0] ? stats.user.images[0].url : 'images/person-circle.svg'}
-                                className='img-fluid rounded-circle h-100 border border-dark'
-                                alt='User Profile Picture'
-                            />
+            {!stats.user ?
+                <div className='d-flex justify-content-center align-items-center' style={{ height: '30em' }}>
+                    <div class="spinner-border text-dark d-flex justify-content-center" role="status" style={{ width: '5em', height: '5em' }}>
+                    </div>
+                </div> :
+                <>
+                    <div className='row'>
+                        {/* User's Profile */}
+                        <div className='card col-md-4 offset-md-1 align-middle text-bg-light mb-5 border-dark mt-5'>
+                            <div className='row align-items-center h-100'>
+                                <div className='col-md-4 h-50'>
+                                    <img src={stats.user?.images[0] ? stats.user.images[0].url : 'images/person-circle.svg'}
+                                        className='img-fluid rounded-circle h-100 border border-dark'
+                                        alt='User Profile Picture'
+                                    />
+                                </div>
+                                <div className='col-md-8'>
+                                    <div className="card-body">
+                                        <h1 className='card-title'>{stats.user.display_name}</h1>
+                                        <p className='card-text'>followers: {stats.user.followers.total}</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className='col-md-8'>
-                            <div className="card-body">
-                                <h1 className='card-title'>{stats.user.display_name}</h1>
-                                <p className='card-text'>followers: {stats.user.followers.total}</p>
+
+                        {/* User's Top Items */}
+                        <div className='col-md-6 card-group mt-5 mb-5'>
+                            <div className='col-md-3 offset-md-1 card text-center text-bg-light border-dark'>
+                                <h4 className='card-header'>Top Track</h4>
+                                <img src={stats.tracks[0]?.images.url}
+                                    alt='Artist Picture'
+                                    className='rounded-circle p-1 mx-auto d-block'
+                                    style={{ width: '75%' }} />
+                                <div className="card-footer">
+                                    <Link href={`/song/${stats.tracks[0].id}`} className='text-decoration-none text-dark'>
+                                        <h5 className="card-title">{stats.tracks[0].name}</h5>
+                                    </Link>
+                                </div>
+                            </div>
+                            <div className='col-md-3 card text-bg-light border-dark d-block'>
+                                <h4 className='card-header text-center'>Top Genre(s)</h4>
+                                {stats.genres
+                                    .filter(genre => genre[1] === Math.max(...stats.genres.map(genre => genre[1])))
+                                    .map(genre =>
+                                        <Link href={`/genre/${genre[0]}`} className='text-decoration-none text-dark'>
+                                            <p key={genre[0]} className='bg-light border border-dark d-inline-block p-2 m-1 rounded' >{genre[0]}</p>
+                                        </Link>
+                                    )}
+                            </div>
+                            <div className='col-md-3 card text-center text-bg-light border-dark'>
+                                <h4 className='card-header'>Top Artist</h4>
+                                <img src={stats.artists[0]?.images.url}
+                                    alt='Artist Picture'
+                                    className='rounded-circle p-1 mx-auto d-block'
+                                    style={{ width: '75%' }} />
+                                <div className="card-footer">
+                                    <Link href={`/artist/${stats.artists[0].id}`} className='text-decoration-none text-dark'>
+                                        <h5 className="card-title">{stats.artists[0].name}</h5>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                {/* User's Top Items */}
-                <div className='col-md-6 card-group mt-5 mb-5'>
-                    <div className='col-md-3 offset-md-1 card text-center text-bg-light border-dark'>
-                        <h4 className='card-header'>Top Track</h4>
-                        <img src={stats.tracks[0]?.images.url}
-                            alt='Artist Picture'
-                            className='rounded-circle p-1 mx-auto d-block'
-                            style={{ width: '75%' }} />
-                        <div className="card-footer">
-                            <Link href={`/song/${stats.tracks[0].id}`} className='text-decoration-none text-dark'>
-                                <h5 className="card-title">{stats.tracks[0].name}</h5>
-                            </Link>
+                    {/* Top songs features average */}
+                    <div className='row text-center border pt-5'>
+                        <h1>Top songs analysis</h1>
+                        <div className='offset-1 col-10 row mt-5'>
+                            {Object.entries(stats.features).map(([name, value]) => <FeatureBar key={name} name={name} value={value} />)}
                         </div>
                     </div>
-                    <div className='col-md-3 card text-bg-light border-dark d-block'>
-                        <h4 className='card-header text-center'>Top Genre(s)</h4>
-                        {stats.genres
-                            .filter(genre => genre[1] === Math.max(...stats.genres.map(genre => genre[1])))
-                            .map(genre =>
-                                <Link href={`/genre/${genre[0]}`} className='text-decoration-none text-dark'>
-                                    <p key={genre[0]} className='bg-light border border-dark d-inline-block p-2 m-1 rounded' >{genre[0]}</p>
+
+                    {/* Top songs list */}
+                    <div className='row text-center border pt-5 pb-5'>
+                        <h1 className='pb-5'>Top Songs</h1>
+                        <div className='offset-1 col-10 row justify-content-center'>
+                            {stats.tracks.map(track =>
+                                <Link href={`/song/${track.id}`} className='text-decoration-none text-dark card col-md-2 m-2 p-0'>
+                                    <img src={track.images.url} className='card-img-top' alt='Album image' />
+                                    <div className='card-body'>
+                                        <h6 className='card-title'>{track.name}</h6>
+                                    </div>
+                                    <div className='card-footer'>
+                                        <p className='text-muted'>{track.artists.toString()}</p>
+                                    </div>
                                 </Link>
                             )}
-                    </div>
-                    <div className='col-md-3 card text-center text-bg-light border-dark'>
-                        <h4 className='card-header'>Top Artist</h4>
-                        <img src={stats.artists[0]?.images.url}
-                            alt='Artist Picture'
-                            className='rounded-circle p-1 mx-auto d-block'
-                            style={{ width: '75%' }} />
-                        <div className="card-footer">
-                            <Link href={`/artist/${stats.artists[0].id}`} className='text-decoration-none text-dark'>
-                                <h5 className="card-title">{stats.artists[0].name}</h5>
-                            </Link>
                         </div>
                     </div>
-                </div>
-            </div>
 
-            {/* Top songs features average */}
-            <div className='row text-center border pt-5'>
-                <h1>Top songs analysis</h1>
-                <div className='offset-1 col-10 row mt-5'>
-                    {Object.entries(stats.features).map(([name, value]) => <FeatureBar key={name} name={name} value={value} />)}
-                </div>
-            </div>
+                    {/* Top genres list */}
+                    <div className='row border text-center pb-5'>
+                        <h1 className='mt-5 mb-5'>Top Genres</h1>
+                        <div className='offset-2 col-8'>
+                            {stats.genres.map(([name]) =>
+                                <Link href={`/genre/${name}`} className='text-decoration-none text-dark'>
+                                    <p key={name} className='bg-light border border-dark d-inline-block p-2 m-1 rounded' >{name}</p>
+                                </Link>
+                            )}
+                        </div>
+                    </div>
 
-            {/* Top songs list */}
-            <div className='row text-center border pt-5 pb-5'>
-                <h1 className='pb-5'>Top Songs</h1>
-                <div className='offset-1 col-10 row justify-content-center'>
-                    {stats.tracks.map(track =>
-                        <Link href={`/song/${track.id}`} className='text-decoration-none text-dark card col-md-2 m-2 p-0'>
-                            <img src={track.images.url} className='card-img-top' alt='Album image' />
-                            <div className='card-body'>
-                                <h6 className='card-title'>{track.name}</h6>
-                            </div>
-                            <div className='card-footer'>
-                                <p className='text-muted'>{track.artists.toString()}</p>
-                            </div>
-                        </Link>
-                    )}
-                </div>
-            </div>
+                    {/* Top Artists list */}
+                    <div className='row border text-center pb-5'>
+                        <h1 className='mt-5 mb-5'>Top Artists</h1>
+                        <div className='offset-1 col-10 row justify-content-center'>
+                            {stats.artists.map(artist =>
+                                <Link href={`/artist/${artist.id}`} className='text-decoration-none text-dark card col-md-2 m-2 p-0'>
+                                    <img src={artist.images ? artist.images.url : 'images/person-circle.svg'} className='card-img-top h-100' alt='Artist image' />
+                                    <div className='card-body d-flex align-items-end'>
+                                        <h6 className='card-title'>{artist.name}</h6>
+                                    </div>
+                                </Link>
+                            )}
+                        </div>
+                    </div>
 
-            {/* Top genres list */}
-            <div className='row border text-center pb-5'>
-                <h1 className='mt-5 mb-5'>Top Genres</h1>
-                <div className='offset-2 col-8'>
-                    {stats.genres.map(([name]) =>
-                        <Link href={`/genre/${name}`} className='text-decoration-none text-dark'>
-                            <p key={name} className='bg-light border border-dark d-inline-block p-2 m-1 rounded' >{name}</p>
-                        </Link>
-                    )}
-                </div>
-            </div>
-
-            {/* Top Artists list */}
-            <div className='row border text-center pb-5'>
-                <h1 className='mt-5 mb-5'>Top Artists</h1>
-                <div className='offset-1 col-10 row justify-content-center'>
-                    {stats.artists.map(artist =>
-                        <Link href={`/artist/${artist.id}`} className='text-decoration-none text-dark card col-md-2 m-2 p-0'>
-                            <img src={artist.images ? artist.images.url : 'images/person-circle.svg'} className='card-img-top h-100' alt='Artist image' />
-                            <div className='card-body d-flex align-items-end'>
-                                <h6 className='card-title'>{artist.name}</h6>
-                            </div>
-                        </Link>
-                    )}
-                </div>
-            </div>
-
-            <button
-                onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-                className='m-5 bg-transparent border-0'
-                style={{ position: 'fixed', bottom: '1%', right: '1%', zIndex: 1000 }}
-            >
-                <img src='images/arrow-up-circle-fill.svg' width='45' />
-            </button>
+                    <button
+                        onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+                        className='m-5 bg-transparent border-0'
+                        style={{ position: 'fixed', bottom: '1%', right: '1%', zIndex: 1000 }}
+                    >
+                        <img src='images/arrow-up-circle-fill.svg' width='45' />
+                    </button>
+                </>}
 
         </Layout>
     )
