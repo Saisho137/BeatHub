acb0e167d3b2595160666afc66adbeffbb5065a6
RottenReality <andresorregolopez@gmail.com>
2023-06-29 11:53:39 -0500

    feat: Added home main buttons

--- frontend_beathub/pages/index.js
+++ frontend_beathub/pages/index.js
@@ -1,10 +1,21 @@
 import Layout from "../components/layout";
+import Link from 'next/link';
+import indexStyles from "../styles/index.module.css";
 
 export default function Home() {
 
   return (
     <Layout>
-      <a>Login with Spotify</a>
+      <div className="container mt-5 pt-5">
+        <div className="row text-center">
+          <div className={`col-md-6 col-sm-12 ${indexStyles.buttonContainer}`}>
+            <Link href="/recommender" className={`${indexStyles.homeButton1}`}>Want to find something new?</Link>
+          </div>
+          <div className={`col-md-6 col-sm-12 ${indexStyles.buttonContainer}`}>
+            <Link href="/game" className={`${indexStyles.homeButton1}`}>Want to play a game?</Link>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
