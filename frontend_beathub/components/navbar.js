1e7eeaa2f3381e71744cead4a710850f938f09f8
RottenReality <andresorregolopez@gmail.com>
2023-10-06 18:32:27 -0500

    fix: redirect_uri changed to vercel

--- frontend_beathub/components/navbar.js
+++ frontend_beathub/components/navbar.js
@@ -54,7 +54,7 @@ const Navbar = () => {
 
   const handleLogin = () => {
     const CLIENT_ID = '39dd8906e8044b7eb1715c1a4a1867e7'
-    const REDIRECT_URI = "http://localhost:3000"||"beat-hub-two.vercel.app/callback"
+    const REDIRECT_URI = "beat-hub-two.vercel.app/callback"
     const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
     const RESPONSE_TYPE = 'token'
     const SCOPE = 'user-read-private playlist-read-private user-read-currently-playing user-follow-read user-top-read playlist-modify-public playlist-modify-private user-library-modify'
